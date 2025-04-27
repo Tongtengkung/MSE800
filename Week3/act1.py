5dba184757b105a3013137b4c483a3f784a34721
Tongtengkung <norrapat.w.tongteng@gmail.com>
2025-04-27 12:20:12 +1200

    update

--- Week3/act1.py
+++ Week3/act1.py
@@ -1,18 +1,22 @@
+class Factorial:
+    def __init__(self, n):
+        self.n = n
+
+    def calculate(self):
+        if self.n < 0:
+            return "Unidentify number lower than zero"
+
+        elif 0 < self.n == 1:
+            return 1
+
+        else:
+            result = 1
+            for i in range(1, self.n + 1):
+                result *= i
+            return result
+
 number = input("input value(PLEASE PUT INTENGER NUMBER):")
+n = int(number)
 
-n = float(number)
-n = round(n)
-def factorial(n):
-    if n < 0:
-        return "Unidentify number lower than zero"
-
-    elif 0 < n <= 1:
-        return 1
-
-    else:
-        result = 1
-        for i in range(1, n + 1):
-            result *= i
-        return result
-
-print(factorial(n))
+factorial = Factorial(n)
+print(f"Factorial of {n} is {factorial.calculate()}")
