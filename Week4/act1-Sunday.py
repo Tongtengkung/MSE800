ca7d4d3976f2384db784c3549340d49cadadd799
Tongtengkung <norrapat.w.tongteng@gmail.com>
2025-05-04 11:35:56 +1200

    Sunday update2

--- Week4/act1-Sunday.py
+++ Week4/act1-Sunday.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
 
-with open('sample_text.txt', 'r') as file:                                  # Open the text file in read mode       
+with open('Week4\sample_text.txt', 'r') as file:                                  # Open the text file in read mode       
+    text_content = file.read()                                              # Read the content of the file into a string                     # Count the occurrences of '__' in the text content
+print(text_content)
+
+with open('Week4\sample_text.txt', 'r') as file:                                  # Open the text file in read mode       
     text_content = file.read()                                              # Read the content of the file into a string
     count_double_underscores = text_content.count('__')                     # Count the occurrences of '__' in the text content
-print(f"Number of '__' in the text file: {count_double_underscores}")       # Print the count of '__' in the text file
+    print(f"Number of '__' in the text file: {count_double_underscores}")
