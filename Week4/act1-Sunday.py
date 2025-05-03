89f960b0586194d116ecd3ee1d73a073d161b5b1
Tongtengkung <norrapat.w.tongteng@gmail.com>
2025-05-04 11:27:00 +1200

    comment change

--- Week4/act1-Sunday.py
+++ Week4/act1-Sunday.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-with open('sample_text.txt', 'r') as file:
-    text_content = file.read()
-    count_double_underscores = text_content.count('__')
-print(f"Number of '__' in the text file: {count_double_underscores}")
+with open('sample_text.txt', 'r') as file:                                  # Open the text file in read mode       
+    text_content = file.read()                                              # Read the content of the file into a string
+    count_double_underscores = text_content.count('__')                     # Count the occurrences of '__' in the text content
+print(f"Number of '__' in the text file: {count_double_underscores}")       # Print the count of '__' in the text file
--- Week4/act2-Sunday.py
+++ Week4/act2-Sunday.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
-parquet_df = pd.read_parquet('Sample_data_2.parquet')
+parquet_df = pd.read_parquet('Sample_data_2.parquet')               # Read the Parquet file into a DataFrame    
 
-num_features = len(parquet_df.columns)
+print(parquet_df.head())                                            # Display the first few rows of the DataFrame                               
+num_features = len(parquet_df.columns)                              # Count the number of features (columns) in the DataFrame       
 
-print(f"Number of features: {num_features}")
+print(f"Number of features: {num_features}")                        # Print the number of features (columns) in the DataFrame
