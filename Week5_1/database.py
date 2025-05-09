ed8849f3c6ccd69acc5cffd6913ea0425d0a1470
Tongtengkung <norrapat.w.tongteng@gmail.com>
2025-05-10 09:54:21 +1200

    updated_v0.1
    initial

---
+++ Week5_1/database.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+def create_connection():                                                #ฟังก์ชันสำหรับสร้างการเชื่อมต่อกับฐานข้อมูล
+    conn = sqlite3.connect("users.db")                                  #สร้างการเชื่อมต่อกับฐานข้อมูล users.db
+    return conn                                                         #ส่งค่าการเชื่อมต่อกลับไปยังฟังก์ชันที่เรียกใช้
+
+def create_table():                                                     #ฟังก์ชันสำหรับสร้างตารางในฐานข้อมูล
+    conn = create_connection()                                          #สร้างการเชื่อมต่อกับฐานข้อมูล
+    cursor = conn.cursor()                                              #ใช้สำหรับการสร้าง cursor object
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            email TEXT NOT NULL UNIQUE
+        )
+    ''')
+    conn.commit()
+    conn.close()
