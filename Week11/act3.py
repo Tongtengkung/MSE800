3c9126d00cd9cb715b458b823d6a5c556efe10ad
Tongtengkung <norrapat.w.tongteng@gmail.com>
2025-06-22 12:14:41 +1200

    updated

---
+++ Week11/act3.py
@@ -0,0 +1,21 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):                                 # test string input 
+    def test_upper(self):                                                   # test 'upper letter'
+        self.assertEqual('foo'.upper(), 'FOO')                              # try on 'foo' to 'FOO'
+
+    def test_isupper(self):                                                 # test is upper is correct?
+        self.assertTrue('FOO'.isupper())                                    # test 'FOO' and must be correct
+        self.assertFalse('Foo'.isupper())                                   # test 'Foo' and must be incorrect because 'oo'
+
+    def test_split(self):                                                   # test split word
+        s = 'hello world'                                                   # set s = string 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])                     # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):                                  
+            s.split(2)                                                      
+
+    def test_isdigit(self):                                                 # test is digit?
+        self.assertTrue('123'.isdigit())                                    # test '123' is digit and must be correct   
+
+if __name__ == '__main__':                                                  # running
+    unittest.main()
